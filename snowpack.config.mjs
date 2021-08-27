b23c1c9a5d1af74fa8ea67749d9136f2be65f7d0
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-08-27 13:38:27 +0000

    change prod optimization configuration

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -27,7 +27,9 @@ export default {
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    bundle: false
+    bundle: true,
+    minify: true,
+    target: "es2018"
   },
   packageOptions: {
     /* ... */
