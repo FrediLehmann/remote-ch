c33ee95fa24ba3f8b4547acf00d9a6d9c8aecbae
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-08-29 12:33:25 +0000

    add noopener

--- src/components/Content/components/SourceLink/SourceLink.tsx
+++ src/components/Content/components/SourceLink/SourceLink.tsx
@@ -7,7 +7,7 @@ import styles from "./sourceLink.module.css"
 const SourceLink = (): JSX.Element => (
   <span className={styles.span}>
     Data source{" "}
-    <Link href="https://remoteok.io/" external target="_blank">
+    <Link href="https://remoteok.io/" external target="_blank" rel="noopener">
       remoteok.io
     </Link>
   </span>
