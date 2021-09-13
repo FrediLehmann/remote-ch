ad1f0096e7046242917ba2e14dd2667b2ead63a9
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-09-13 14:52:54 +0000

    add watermark

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 
+import { Logo } from "./Logo"
 import styles from "./header.module.css"
 
 const Header = (): JSX.Element => (
   <header className={styles.header}>
+    <Logo />
     <h1 className={styles.h1}>remote-ch</h1>
   </header>
 )
